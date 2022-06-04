a4fff14d12136ee8e383b5daa7afc8fb621a67de
A00815749 <56553263+A00815749@users.noreply.github.com>
2022-06-04 15:41:39 -0500

    outer program shell grammar
    missing neuralgic aux functions

--- myLexerParserv2.py
+++ myLexerParserv2.py
@@ -3,6 +3,9 @@
 #Parser by Andres Carlos Barrera Basilio A00815749
 #Parser using the above LExer and PLY
 
+################## THE MIR LEXER AND PARSER #############################
+
+
 #Librerias para importar en el Lexer-Parser
 from re import split
 import os
@@ -99,3 +102,240 @@ PARAMSINTcounter = 30000 - 1
 PARAMSFLOATcounter = 33000 - 1
 PARAMSCHARcounter = 36000 - 1 # BLOCK of 4000 spaces 
 POINTERScounter = 40000 - 1 # LAST BLOCK
+
+
+############### QUADRUPLE CLASS FOR STORING THE COMPILER OPERATIONS ############
+
+class Quadruple :
+    def __init__(self, operator,LeftOperand,RightOperand,result):
+        global QUADSlist
+        self.QUADcounter = len(QUADSlist) + 1 # The number of the quadruple, so that we can have GOTO and derived functions
+        self.operator = operator
+        self.LeftOperand = LeftOperand
+        self.RightOperand = RightOperand
+        self.result = result
+
+#### SEMANTIC CUBE CLASS OBJECT, A SENSOR THAR CHECKS OPERATIONS BETWEEN THE SIMPLE DATATYPES #############
+semantics = Semanticcube()
+
+########################################################################################################################################
+
+#---------------------------------------------------NEURALGIC FUNCTIONS AUX ------------------------------------------------------------
+#---------------------------------------------------NEURALGIC FUNCTIONS AUX ------------------------------------------------------------
+#---------------------------------------------------NEURALGIC FUNCTIONS AUX ------------------------------------------------------------
+#---------------------------------------------------NEURALGIC FUNCTIONS AUX ------------------------------------------------------------
+#---------------------------------------------------NEURALGIC FUNCTIONS AUX ------------------------------------------------------------
+
+
+
+
+########### GLOBAL AUXILIAR METHODS FOR NEURALGIC POINTS MANIPULATIONS ############
+
+#~~~~~~~IN PROGRESSS~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#~~~~~~~IN PROGRESSS~~~~~~~~~~~
+
+
+########################################################################################################################################
+
+#---------------------------------------------------THE LEXER -------------------------------------------------------------------------- 
+#---------------------------------------------------THE LEXER -------------------------------------------------------------------------- 
+#---------------------------------------------------THE LEXER -------------------------------------------------------------------------- 
+#---------------------------------------------------THE LEXER -------------------------------------------------------------------------- 
+#---------------------------------------------------THE LEXER -------------------------------------------------------------------------- 
+#---------------------------------------------------THE LEXER -------------------------------------------------------------------------- 
+
+reserved = {
+    'Program' : 'PROGRAM', # program reserved word
+    'main' : 'MAIN', # main reserved word
+    'function' : 'FUNCTION', # function reserved word 
+    'VARS' : 'VARS', # VARS reserved word
+    'int' : 'INT', # int reserved word
+    'float' : 'FLOAT', # flot reserved word
+    'char' : 'CHAR', # char reserved word
+    'str' : 'STR', # STR reserved word
+    'return' : 'RETURN', # return reserved word
+    'read' : 'READ', # read reserved word
+    'write' : 'WRITE', # write reserved word
+    'and' : 'AND', # and reserved word
+    'or' : 'OR', # or reserved word
+    'if' : 'IF', # if reserved word
+    'then' : 'THEN', # then reserved word
+    'else' : 'ELSE',  # else reserved word
+    'while' : 'WHILE', # while reserved word
+    'do' : 'DO', # do reserved word
+    'for' : 'FOR', # for reserved word
+    'to' : 'TO', # to reserved word
+    'void' : 'VOID', # void reserved word
+    'true' : 'TRUE', # TRUE reserved word
+    'false' : 'FALSE', # FALSE reserved word
+    'media' : 'MEDIA', # special function average
+    'mediana': 'MEDIANA', # special function median
+    'moda' : 'MODA', # special function mode
+    'varianza' : 'VARIANZA', # special function variance
+    'stdev' : 'STDEV', # special function simple regression
+    'plotxy' : 'PLOTXY', # special function plot two data columns
+    } #dont put the previous reserved words as ID types, this handles that
+
+
+##### THE LIST OF TOKENS IN MIR LANGUAGE
+tokens = [
+    'STRING', # String token
+    'ID', # ID token
+    'PLUS', # + symbol
+    'REST', # - symbol
+    'TIMES', # * symbol
+    'DIVIDE', # / symbol
+    'GREATER', # > symbol
+    'GREATERAND', # >= symbol
+    'LESSER', # < symbol
+    'LESSERAND', # <= symbol
+    'SAME', # == symbol
+    'NOTSAME', # <> symbol
+    'NOT', # ! symbol
+    'EQUAL', # = symbol
+    'LEFTBR', # { symbol
+    'RIGHTBR', # } symbol
+    'LEFTPAR', # ( symbol
+    'RIGHTPAR', # ) symbol
+    'LEFTSQR', # [ symbol
+    'RIGHTSQR', # ] symbol
+    'COLON', # : symbol
+    'SEMICOLON', # ; symbol
+    'COMMA', # , symbol
+    'CTEINT', # constant int
+    'CTEFLOAT', # constant float
+    'CTECHAR', # constant char
+] + list(reserved.values())
+
+##### THE DEFINITION OF THE TOKENS
+#---SYMBOLS
+
+t_ignore = ' \t'
+t_SEMICOLON = r'\;'
+t_COLON = r'\:'
+t_COMMA = r'\,'
+t_EQUAL = r'\='
+t_SAME = r'\=\='
+t_LEFTPAR = r'\('
+t_RIGHTPAR = r'\)'
+t_LEFTBR = r'\{'
+t_RIGHTBR = r'\}'
+t_LEFTSQR = r'\['
+t_RIGHTSQR = r'\]'
+t_STRING = r'\".*\"'
+t_PLUS  = r'\+'
+t_REST = r'\-'
+t_TIMES = r'\*'
+t_DIVIDE = r'\/'
+t_GREATER = r'\>'
+t_GREATERAND = r'\>\='
+t_LESSER = r'\<'
+t_LESSERAND = r'\<\='
+t_NOTSAME = r'\<\>'
+t_NOT = r'\!'
+t_CTECHAR =r"\'.\'"
+
+#----COMPLEX DEFINITIONS
+
+def t_CTEFLOAT(t):
+    r'-?\d+\.\d+' # able to accept sign symbols, and .97 (numbers without the integer part)
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("Float value too large %d", t.value)
+        t.value = 0
+    return t
+
+def t_CTEINT(t):
+    r'-?\d+' # taking account if sign symbol is present
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9]*'
+    t.type = reserved.get(t.value, 'ID') 
+    return t
+
+
+# every symbol that doesn't match with almost one of the previous tokens is considered an error
+# modification so that all errors can be processed and debugged with the error catcher below
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    print("ERROR with illegal character (lexer) at: '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+###################### BUILDING THE LEXER #########################
+import ply.lex as lex
+lexer = lex.lex()
+
+
+
+
+
+########################################################################################################################################
+
+#---------------------------------------------------THE GRAMMAR ------------------------------------------------------------------------
+#---------------------------------------------------THE GRAMMAR ------------------------------------------------------------------------
+#---------------------------------------------------THE GRAMMAR ------------------------------------------------------------------------
+#---------------------------------------------------THE GRAMMAR ------------------------------------------------------------------------
+#---------------------------------------------------THE GRAMMAR ------------------------------------------------------------------------
+#---------------------------------------------------THE GRAMMAR ------------------------------------------------------------------------ 
+
+
+
+
+#--------------OUTER INITIAL SHELL--------------#
+#functions = modules
+
+def p_PROGRAM(p): #PROGRAM SHELL LOGIC
+    '''
+    program : PROGRAM neuraladdfuncdir varsgl modules MAIN LEFTPAR RIGHTPAR LEFTBR neuralmainjump statutes RIGHTBR
+    '''
+    print ('Llego al final de la gramatica, aceptado \n')
+    global GLOBALINTcounter,GLOBALFLOATcounter,GLOBALCHARcounter
+    global TEMPINTcounter,TEMPFLOATcounter,TEMPCHARcounter,TEMPBOOLcounter
+    global CONSTINTcounter,CONSTFLOATcounter,CONSTCHARcounter,POINTERScounter
+
+    actualmodulename = p[2] # GET THE NAME OF THE PROGRAM, WHICH WILL BE STORED IN THE DIRFUNC
+
+    TABLEof_functions[actualmodulename]['Intnumbers'] = (GLOBALINTcounter-(1000-1)) + (TEMPINTcounter - (13000-1))
+    TABLEof_functions[actualmodulename]['Floatnumbers'] = (GLOBALFLOATcounter - (3000 - 1)) + (TEMPFLOATcounter - (15000-1))
+    TABLEof_functions[actualmodulename]['Charnumbers'] = (GLOBALCHARcounter-(5000-1)) + (TEMPCHARcounter - (17000-1))
+    TABLEof_functions[actualmodulename]['Boolnumbers'] = (TEMPBOOLcounter-(19000-1))
+    TABLEof_functions[actualmodulename]['Pointernumbers'] = (POINTERScounter-(40000-1))
+    #STORING THE VARIABLE NUMBERS BY SUBSTRACTING THE INITIAL MEMORY ALLOCATIONS TO THE FINAL VARIABLES COUNTERS
+
+def p_NEURALADDFUNCDIR(p): #NEURALGIC POINT THAT SAVES THE MAIN PROGRAM, STORING ITS QUADRUPLE PLACEMENT
+    '''
+    neuraladdfuncdir : ID SEMICOLON
+    '''
+    global CURRENTcontext,GLOBALvar_set,QUADSlist,HASHofoperatorsinquads,Pjumps,CONSTANTSvar_set
+    p[0] = p[1] # SKIP THE TOKEN
+    #### AUXILIAR insert in the table function###
+    QUADSlist.append(Quadruple(HASHofoperatorsinquads['GOTO'],-1,-1,-999))
+    Pjumps.append(len(QUADSlist))
+    #### AUXILIAR set the virtual address of constants 0 and 1, we will need it##
